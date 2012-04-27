564233f9b44e8a561733bf97f7510d1aa406e94d
Paige Ponzeka <paigepon@gmail.com>
2012-04-27 08:15:41 -0400

    Intial creating on files with code for the web scraping already in place in the home/indew not the right home but we'll fix that later

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glee::Application.config.secret_token = '0adf16d33d8643ce49877726cd34d0f1a83a08f159cb40b7e86db2813ed83f9d34f1a8e5562fe2a1c7be62c37b66ed6bfe6f3f114bdfab795dba78f75cd84974'
