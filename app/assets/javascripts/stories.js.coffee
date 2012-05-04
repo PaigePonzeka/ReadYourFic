0d373ae8ae21be790f791f83011ceea67331484c
Paige Ponzeka <paigepon@gmail.com>
2012-05-03 22:35:09 -0400

    populating the filter fields based on the selected fields

--- app/assets/javascripts/stories.js.coffee
+++ app/assets/javascripts/stories.js.coffee
@@ -1,7 +1,61 @@
 
 
 $(document).ready ->
+
   window.current_controller = $('body').attr('id')
   console.log(current_controller)
   if(current_controller == "stories")
+
+    # Intialize the filters
+    intializeFilters()
+
+    # intialize the chosen library
     $(".chzn-select").chosen()
+
+
+# TODO maybe look into making a stories class object for funtions
+
+# Get the url variables from
+getUrlVars = ->
+  vars = {}
+  characters = []
+  themes = []
+  parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/g, (m, key, value) ->
+    vars[key]=value
+    if key == "character%5B%5D"
+      characters.push(value)
+    else if key == "themes%5B%5D"
+      themes.push(value)
+  )
+  vars["characters"] = characters
+  vars["themes"] = themes
+  vars
+
+
+# Intialize selected fields based on the params in the url
+intializeFilters= ->
+
+  # get the parameters from the url
+  url_vars = getUrlVars()
+  console.log url_vars
+  console.log url_vars['search']
+  console.log url_vars['character%5B%5D']
+  console.log url_vars['themes%5B%5D']
+
+  # set the content for the search bar
+  if url_vars['search']
+    $('#search').val(url_vars['search'])
+
+  # set the selected values for the characters
+  characters = url_vars['characters']
+  if(characters.length > 0)
+    for character of characters
+      $("#character option[value=" + characters[character]+"]").attr("selected","selected")
+
+
+  # set the selected values for the characters
+  themes = url_vars['themes']
+  if(themes.length > 0)
+    for theme of themes
+      $("#themes option[value=" + themes[theme]+"]").attr("selected","selected")
+
--- app/assets/javascripts/stories.js.js
+++ app/assets/javascripts/stories.js.js
@@ -1,9 +1,55 @@
 (function() {
+  var getUrlVars, intializeFilters;
 
   $(document).ready(function() {
     window.current_controller = $('body').attr('id');
     console.log(current_controller);
-    if (current_controller === "stories") return $(".chzn-select").chosen();
+    if (current_controller === "stories") {
+      intializeFilters();
+      return $(".chzn-select").chosen();
+    }
   });
+
+  getUrlVars = function() {
+    var characters, parts, themes, vars;
+    vars = {};
+    characters = [];
+    themes = [];
+    parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/g, function(m, key, value) {
+      vars[key] = value;
+      if (key === "character%5B%5D") {
+        return characters.push(value);
+      } else if (key === "themes%5B%5D") {
+        return themes.push(value);
+      }
+    });
+    vars["characters"] = characters;
+    vars["themes"] = themes;
+    return vars;
+  };
+
+  intializeFilters = function() {
+    var character, characters, theme, themes, url_vars, _results;
+    url_vars = getUrlVars();
+    console.log(url_vars);
+    console.log(url_vars['search']);
+    console.log(url_vars['character%5B%5D']);
+    console.log(url_vars['themes%5B%5D']);
+    if (url_vars['search']) $('#search').val(url_vars['search']);
+    characters = url_vars['characters'];
+    if (characters.length > 0) {
+      for (character in characters) {
+        $("#character option[value=" + characters[character] + "]").attr("selected", "selected");
+      }
+    }
+    themes = url_vars['themes'];
+    if (themes.length > 0) {
+      _results = [];
+      for (theme in themes) {
+        _results.push($("#themes option[value=" + themes[theme] + "]").attr("selected", "selected"));
+      }
+      return _results;
+    }
+  };
 
 }).call(this);
